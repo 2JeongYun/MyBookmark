a3d069d6be032a7515dfcd618002daa6aeacc2f2
2JeongYun <leejeongyun828@gmail.com>
2021-04-13 21:55:58 +0900

    Implement home

---
+++ src/main/resources/static/js/home.js
@@ -0,0 +1,29 @@
+let home = {
+    init: function () {
+        let _this = this;
+        $('#category-save-btn').on('click', function () {
+            _this.categorySave();
+        });
+    },
+
+    categorySave: function () {
+        let data = {
+            name: $('#name').val(),
+            color: $('#color').val()
+        };
+
+        $.ajax({
+           type: 'POST',
+           url: '/api/v1/category',
+           dataType: 'json',
+           contentType: 'application/json; charset=utf-8',
+           data: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 등록되었습니다.');
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    }
+}
+
+home.init();
