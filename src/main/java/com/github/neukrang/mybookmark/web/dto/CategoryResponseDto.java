9494051940555e5c8e9d603c1e9784003959c60f
2JeongYun <leejeongyun828@gmail.com>
2021-04-14 18:00:32 +0900

    Implement CategoryResponseDto

---
+++ src/main/java/com/github/neukrang/mybookmark/web/dto/CategoryResponseDto.java
@@ -0,0 +1,23 @@
+package com.github.neukrang.mybookmark.web.dto;
+
+import com.github.neukrang.mybookmark.domain.category.Category;
+import com.github.neukrang.mybookmark.domain.bookmark.Bookmark;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class CategoryResponseDto {
+
+    private String name;
+    private String color;
+    private List<Bookmark> bookmarks;
+    private int openCount;
+
+    public CategoryResponseDto(Category Entity) {
+        this.name = Entity.getName();
+        this.color = Entity.getColor();
+        this.bookmarks = Entity.getBookmarks();
+        this.openCount = Entity.getOpenCount();
+    }
+}
