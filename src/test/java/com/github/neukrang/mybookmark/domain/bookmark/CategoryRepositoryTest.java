e850e67bee558136e682a541d3088db85e203c27
2JeongYun <leejeongyun828@gmail.com>
2021-04-14 18:04:26 +0900

    Implement CategoryRepositoryTest

---
+++ src/test/java/com/github/neukrang/mybookmark/domain/bookmark/CategoryRepositoryTest.java
@@ -0,0 +1,67 @@
+package com.github.neukrang.mybookmark.domain.bookmark;
+
+import com.github.neukrang.mybookmark.domain.category.Category;
+import com.github.neukrang.mybookmark.domain.category.CategoryRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CategoryRepositoryTest {
+
+    @Autowired
+    private BookmarkRepository bookmarkRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @After
+    public void clean() {
+        bookmarkRepository.deleteAll();
+        categoryRepository.deleteAll();
+    }
+
+    @Test
+    public void isMapped() {
+        Category category = categoryRepository.save(Category.builder()
+                .name("test category")
+                .color("test color")
+                .build());
+
+        int bookmarkCount = 2;
+        for (int i = 0; i < bookmarkCount; i++) {
+            saveMockBookmarks(category, bookmarkRepository);
+        }
+        saveMockCategory(categoryRepository, bookmarkRepository);
+
+        categoryRepository.findById(category.getId());
+        assertThat(category.getBookmarks().size()).isEqualTo(bookmarkCount);
+    }
+
+    public void saveMockBookmarks(Category category, BookmarkRepository bookmarkRepository) {
+        bookmarkRepository.save(Bookmark.builder()
+                .address("test1")
+                .category(category)
+                .alias("test1")
+                .color("test1")
+                .description("test1")
+                .build());
+    }
+    public void saveMockCategory(CategoryRepository categoryRepository, BookmarkRepository bookmarkRepository) {
+        Category category = categoryRepository.save(Category.builder()
+                .name("Mock")
+                .color("Mock")
+                .build());
+
+        bookmarkRepository.save(Bookmark.builder()
+                .category(category)
+                .address("Mock")
+                .build());
+    }
+}
