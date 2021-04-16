687b7b12c5c4f03350edcfabdd77052ed16fb26d
2JeongYun <leejeongyun828@gmail.com>
2021-04-16 23:09:49 +0900

    Set H2-testDB initialize

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO category(name, color, open_count) VALUES ('first', '#000000', 1);
+INSERT INTO category(name, color, open_count) VALUES ('second', '#000000', 2);
+INSERT INTO category(name, color, open_count) VALUES ('third', '#000000', 3);
+INSERT INTO category(name, color, open_count) VALUES ('fourth', '#000000', 4);
