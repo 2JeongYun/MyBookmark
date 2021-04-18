5ba22573fed7bf18b5a4751d7056bcd9efb138ce
2JeongYun <leejeongyun828@gmail.com>
2021-04-18 22:45:55 +0900

    Add BookmarkResponseDto list to CategoryResponseDto

--- src/main/java/com/github/neukrang/mybookmark/web/dto/CategoryResponseDto.java
+++ src/main/java/com/github/neukrang/mybookmark/web/dto/CategoryResponseDto.java
@@ -3,18 +3,25 @@ package com.github.neukrang.mybookmark.web.dto;
 import com.github.neukrang.mybookmark.domain.category.Category;
 import lombok.Getter;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter
 public class CategoryResponseDto {
 
     private Long id;
     private String name;
     private String color;
+    private List<BookmarkResponseDto> bookmarkResponseDtoList;
     private int openCount;
 
-    public CategoryResponseDto(Category Entity) {
-        this.id = Entity.getId();
-        this.name = Entity.getName();
-        this.color = Entity.getColor();
-        this.openCount = Entity.getOpenCount();
+    public CategoryResponseDto(Category entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.color = entity.getColor();
+        this.openCount = entity.getOpenCount();
+        this.bookmarkResponseDtoList = entity.getBookmarks().stream()
+                .map(BookmarkResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
