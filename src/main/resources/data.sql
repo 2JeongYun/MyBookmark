9085d623d3b2379df65f53bda103b2b03366966a
2JeongYun <leejeongyun828@gmail.com>
2021-04-18 22:43:17 +0900

    Add bookmark data to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,8 @@ INSERT INTO category(name, color, open_count) VALUES ('first', '#000000', 1);
 INSERT INTO category(name, color, open_count) VALUES ('second', '#000000', 2);
 INSERT INTO category(name, color, open_count) VALUES ('third', '#000000', 3);
 INSERT INTO category(name, color, open_count) VALUES ('fourth', '#000000', 4);
+
+INSERT INTO bookmark(address, alias, color, category_id, open_count) VALUES ('address1-1', 'alias', '#000000', 1, 0);
+INSERT INTO bookmark(address, alias, color, category_id, open_count) VALUES ('address1-2', 'alias', '#000000', 1, 0);
+INSERT INTO bookmark(address, alias, color, category_id, open_count) VALUES ('address3-1', 'alias', '#000000', 3, 0);
+INSERT INTO bookmark(address, alias, color, category_id, open_count) VALUES ('address2-1', 'alias', '#000000', 2, 0);
