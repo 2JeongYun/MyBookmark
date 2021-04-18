8eb491ba8e8f505823970c266b37996a1841071c
2JeongYun <leejeongyun828@gmail.com>
2021-04-18 15:02:01 +0900

    Remove Bookmarks from CategoryResponse

--- src/main/java/com/github/neukrang/mybookmark/web/dto/CategoryResponseDto.java
+++ src/main/java/com/github/neukrang/mybookmark/web/dto/CategoryResponseDto.java
@@ -1,10 +1,7 @@
 package com.github.neukrang.mybookmark.web.dto;
 
 import com.github.neukrang.mybookmark.domain.category.Category;
-import com.github.neukrang.mybookmark.domain.bookmark.Bookmark;
 import lombok.Getter;
-
-import java.util.List;
 
 @Getter
 public class CategoryResponseDto {
@@ -12,14 +9,12 @@ public class CategoryResponseDto {
     private Long id;
     private String name;
     private String color;
-    private List<Bookmark> bookmarks;
     private int openCount;
 
     public CategoryResponseDto(Category Entity) {
         this.id = Entity.getId();
         this.name = Entity.getName();
         this.color = Entity.getColor();
-        this.bookmarks = Entity.getBookmarks();
         this.openCount = Entity.getOpenCount();
     }
 }
