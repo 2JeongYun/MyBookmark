93ce9472d164a1cd9578ba4417110f02c13636f7
2JeongYun <leejeongyun828@gmail.com>
2021-04-26 20:41:02 +0900

    Fixme Login function

---
+++ src/main/java/com/github/neukrang/mybookmark/config/auth/CustomOAuth2UserService.java
@@ -0,0 +1,59 @@
+package com.github.neukrang.mybookmark.config.auth;
+
+import com.github.neukrang.mybookmark.domain.user.User;
+import com.github.neukrang.mybookmark.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        // 로그인 진행하는 서비스 이름 (네이버, 구글, 카카오.. etc)
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        // 로그인 진행 시 키가 되는 필드값, 즉 계정 고유의 ID 값을 서버가 줌 (PK)
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
+                .getUserInfoEndpoint().getUserNameAttributeName();
+
+        // OAuth2UserService 를 통해 가져온 OAuth2User 의 속성을 담을 클래스
+        OAuthAttributes attributes = OAuthAttributes
+                .of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+        // 가져온 속성들을 저장, 혹은 업데이트
+        User user = saveOrUpdate(attributes);
+
+        // 세션에 유저를 저장한다. SessionUser 는 User 의 Dto 클래스이다.
+        httpSession.setAttribute("user", new SessionUser(user));
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey());
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName()))
+                .orElse(attributes.toEntity());
+
+        return userRepository.save(user);
+    }
+}
